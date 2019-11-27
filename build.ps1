d799a370be0f320d1708f9aaeef1d9d0f58344e9
Mateus Dalepiane <mdalepiane@gmail.com>
2019-11-27 01:43:16 -0300

    Fix build script for windows and linux

--- build.ps1
+++ build.ps1
@@ -3,10 +3,10 @@ $msbuild='C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBui
 
 # This could be improved to automatically detect current container OS
 $container_os="windows"
-if ($1 -eq "linux")
+if ($Args[0] -eq "linux")
 {
     $container_os="linux"
 }
 
-$dacpac_file="db-testproject\db-testproject\db-artifacts\db-testproject.dacpac"
+$dacpac_file="db-testproject/db-testproject/db-artifacts/db-testproject.dacpac"
 docker build -t db-testproject -f $container_os\Dockerfile --build-arg DACPAC_FILE=$dacpac_file --build-arg TARGET_DB_NAME="db-testproject" .
