2944d908c10bdac013099f7ef95982903d7a5e0f
Mateus Dalepiane <mdalepiane@gmail.com>
2019-11-26 23:42:56 -0300

    Move Dockerfiles to OS folders

--- build.ps1
+++ build.ps1
@@ -1,5 +1,12 @@
 $msbuild='C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe'
-& $msbuild .\db-testproject\db-testproject.sln /p:OutDir='db-artifacts'
+& $msbuild db-testproject\db-testproject.sln /p:OutDir='db-artifacts'
 
-$dacpac_file="db-testproject/db-testproject/db-artifacts/db-testproject.dacpac"
-docker build -t db-testproject --build-arg DACPAC_FILE=$dacpac_file . --build-arg TARGET_DB_NAME="db-testproject"
+# This could be improved to automatically detect current container OS
+$container_os="windows"
+if ($1 -eq "linux")
+{
+    $container_os="linux"
+}
+
+$dacpac_file="db-testproject\db-testproject\db-artifacts\db-testproject.dacpac"
+docker build -t db-testproject -f $container_os\Dockerfile --build-arg DACPAC_FILE=$dacpac_file --build-arg TARGET_DB_NAME="db-testproject" .
